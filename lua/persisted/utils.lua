769c1308311e4093260ca0d5d465a77ab3125ca3
olimorris <olimorris@users.noreply.github.com>
2022-11-16 13:43:29 +0000

    chore: cleanup utils file

--- lua/persisted/utils.lua
+++ lua/persisted/utils.lua
@@ -1,35 +1,22 @@
 local M = {}
 local e = vim.fn.fnameescape
 
---- Split a string into a table
----@param input string
----@param sep string
----@return table
-function M.split_str(input, sep)
-  if sep == nil then
-    sep = "%s"
-  end
-  local t = {}
-  for str in string.gmatch(input, "([^" .. sep .. "]+)") do
-    table.insert(t, str)
-  end
-  return t
-end
-
---- Get the last element in a table
----@param table table
----@return string
-function M.get_last_item(table)
-  for i, v in pairs(table) do
-    last = #table - 0
-  end
-  return table[last]
+---Print an error message
+--@param msg string
+--@param error string
+--@return string
+local function echoerr(msg, error)
+  vim.api.nvim_echo({
+    { "[persisted.nvim]: ", "ErrorMsg" },
+    { msg, "WarningMsg" },
+    { error, "Normal" },
+  }, true, {})
 end
 
 --- Escape special pattern matching characters in a string
 ---@param input string
 ---@return string
-function M.escape_pattern(input)
+local function escape_pattern(input)
   local magic_chars = { "%", "(", ")", ".", "+", "-", "*", "?", "[", "^", "$" }
 
   for _, char in ipairs(magic_chars) do
@@ -37,6 +24,17 @@ function M.escape_pattern(input)
   end
 
   return input
+end
+
+--- Get the last element in a table
+---@param table table
+---@return string
+function M.get_last_item(table)
+  local last
+  for i, v in pairs(table) do
+    last = #table - 0
+  end
+  return table[last]
 end
 
 ---Check if a target directory exists in a given table
@@ -47,7 +45,7 @@ function M.dirs_match(dir, dirs_table)
   dir = vim.fn.expand(dir)
   return dirs_table
     and next(vim.tbl_filter(function(pattern)
-      return dir:find(M.escape_pattern(vim.fn.expand(pattern)))
+      return dir:find(escape_pattern(vim.fn.expand(pattern)))
     end, dirs_table))
 end
 
@@ -61,17 +59,6 @@ function M.get_dir_pattern()
   return pattern
 end
 
----Print an error message
---@param msg string
---@param error string
---@return string
-function M.echoerr(msg, error)
-  vim.api.nvim_echo({
-    { "[persisted.nvim]: ", "ErrorMsg" },
-    { msg, "WarningMsg" },
-    { error, "Normal" },
-  }, true, {})
-end
 
 ---Load the given session
 ---@param session string
@@ -85,7 +72,7 @@ function M.load_session(session, before_callback, after_callback, silent)
 
     local ok, result = pcall(vim.cmd, (silent and "silent " or "") .. "source " .. e(session))
     if not ok then
-      return M.echoerr("Error loading the session! ", result)
+      return echoerr("Error loading the session! ", result)
     end
 
     if type(after_callback) == "function" then
