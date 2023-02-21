452c36a3f91e5b2e87d93ec13b9ac9e63bc2b0bc
olimorris <olimorris@users.noreply.github.com>
2023-02-21 16:01:15 +0000

    chore: clean up config file

--- lua/persisted/config.lua
+++ lua/persisted/config.lua
@@ -7,17 +7,21 @@ local defaults = {
   silent = false, -- silent nvim message when sourcing session file
   use_git_branch = false, -- create session files based on the branch of the git enabled repository
   branch_separator = "@@", -- string used to separate session directory name from branch name
+
   autosave = true, -- automatically save session files when exiting Neovim
   should_autosave = nil, -- function to determine if a session should be autosaved
+  before_save = nil, -- function to run before the session is saved to disk
+  after_save = nil, -- function to run after the session is saved to disk
+
   autoload = false, -- automatically load the session for the cwd on Neovim startup
   on_autoload_no_session = nil, -- function to run when `autoload = true` but there is no session to load
+  before_source = nil, -- function to run before the session is sourced
+  after_source = nil, -- function to run after the session is sourced
+
   follow_cwd = true, -- change session file name with changes in current working directory
   allowed_dirs = nil, -- table of dirs that the plugin will auto-save and auto-load from
   ignored_dirs = nil, -- table of dirs that are ignored for auto-saving and auto-loading
-  before_save = nil, -- function to run before the session is saved to disk
-  after_save = nil, -- function to run after the session is saved to disk
-  before_source = nil, -- function to run before the session is sourced
-  after_source = nil, -- function to run after the session is sourced
+
   telescope = { -- options for the telescope extension
     before_source = nil, -- function to run before the session is sourced via telescope
     after_source = nil, -- function to run after the session is sourced via telescope
