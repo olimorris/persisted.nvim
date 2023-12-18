ab7040d8c003a83931c12b9ebf935f766b5c4564
olimorris <olimorris@users.noreply.github.com>
2023-12-18 23:49:07 +0000

    chore: formatting

--- tests/autoload/autoload_session_spec.lua
+++ tests/autoload/autoload_session_spec.lua
@@ -23,9 +23,9 @@ describe("Autoloading", function()
     vim.defer_fn(function()
       coroutine.resume(co)
     end, 2000)
-    
-    local fp_sep = vim.loop.os_uname().sysname:lower():match('windows') and '\\' or '/' -- \ for windows, mac and linux both use \
-    
+
+    local fp_sep = vim.loop.os_uname().sysname:lower():match("windows") and "\\" or "/" -- \ for windows, mac and linux both use \
+
     local session_dir = vim.fn.getcwd() .. "/test/dummy_data/"
     require("persisted").setup({
       save_dir = session_dir,
@@ -33,16 +33,15 @@ describe("Autoloading", function()
       autosave = true,
       ignored_dirs = {
         -- Setting the parent of our current to an ignored directory
-        { 
+        {
           string.format("%s%s..%s", vim.fn.getcwd(), fp_sep, fp_sep),
-          exact = true
-        }
-      }
+          exact = true,
+        },
+      },
     })
     coroutine.yield()
 
     local content = vim.fn.getline(1, "$")
     assert.equals("If you're reading this, I guess auto-loading works", content[1])
-
   end)
 end)
