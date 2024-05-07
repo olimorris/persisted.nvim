14b7022f646f1e0f999fd77bc224fbeba78be98a
Rémi Labeyrie <calops@tocards.net>
2024-05-07 11:45:51 +0200

    fix: remove unused last_modified property

--- lua/persisted/init.lua
+++ lua/persisted/init.lua
@@ -367,18 +367,11 @@ function M.list()
       dir_path = session_name
     end
 
-    local f = io.popen("stat -c %x " .. session)
-    local last_modified = ""
-    if f then
-      last_modified = f:read()
-    end
-
     table.insert(sessions, {
       ["name"] = session_name,
       ["file_path"] = session,
       ["branch"] = branch,
       ["dir_path"] = dir_path,
-      ["last_modified"] = last_modified,
     })
   end
 
