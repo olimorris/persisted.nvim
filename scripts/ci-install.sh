d4e235175352f2c2eaa10f1b9c76234e21889efc
Oli Morris <olimorris@users.noreply.github.com>
2026-01-14 15:34:05 +0000

    ci

--- scripts/ci-install.sh
+++ scripts/ci-install.sh
@@ -5,20 +5,51 @@
 set -e
 
 NVIM_TAG=${NVIM_TAG-nightly}
+NVIM_TAG_WITH_V=$NVIM_TAG
+if [[ $NVIM_TAG_WITH_V != v* ]]; then
+  NVIM_TAG_WITH_V="v${NVIM_TAG_WITH_V}"
+fi
+
+download_release() {
+  local asset=$1
+  local url_base="https://github.com/neovim/neovim/releases/download"
+  local url_primary="${url_base}/${NVIM_TAG}/${asset}"
+  local url_fallback="${url_base}/${NVIM_TAG_WITH_V}/${asset}"
+
+  if command -v curl >/dev/null 2>&1; then
+    curl -fL "$url_primary" -o "$asset" || curl -fL "$url_fallback" -o "$asset"
+  else
+    wget "$url_primary" -O "$asset" || wget "$url_fallback" -O "$asset"
+  fi
+}
+
+download_release_first() {
+  local asset
+  for asset in "$@"; do
+    if download_release "$asset"; then
+      echo "$asset"
+      return 0
+    fi
+  done
+  return 1
+}
 
 os=$(uname -s)
 if [[ $os == Linux ]]; then
-  wget "https://github.com/neovim/neovim/releases/download/${NVIM_TAG}/nvim-linux-x86_64.tar.gz"
-  tar -zxf nvim-linux-x86_64.tar.gz
-  sudo ln -s "$PWD"/nvim-linux-x86_64/bin/nvim /usr/local/bin
-  rm -rf "$PWD"/nvim-linu-x86_x64/lib/nvim/parser
+  archive=$(download_release_first "nvim-linux-x86_64.tar.gz" "nvim-linux64.tar.gz")
+  tar -zxf "$archive"
+  dir="${archive%.tar.gz}"
+  sudo ln -s "$PWD/$dir/bin/nvim" /usr/local/bin
+  rm -rf "$PWD/$dir/lib/nvim/parser"
 elif [[ $os == Darwin ]]; then
   RELEASE_NAME="nvim-macos-$(uname -m)"
-  curl -L "https://github.com/neovim/neovim/releases/download/${NVIM_TAG}/$RELEASE_NAME.tar.gz" | tar -xz
-  sudo ln -s "$PWD/$RELEASE_NAME/bin/nvim" /usr/local/bin
-  rm -rf "$PWD/$RELEASE_NAME/lib/nvim/parser"
+  archive=$(download_release_first "$RELEASE_NAME.tar.gz" "nvim-macos.tar.gz")
+  tar -zxf "$archive"
+  dir="${archive%.tar.gz}"
+  sudo ln -s "$PWD/$dir/bin/nvim" /usr/local/bin
+  rm -rf "$PWD/$dir/lib/nvim/parser"
 else
-  curl -L "https://github.com/neovim/neovim/releases/download/${NVIM_TAG}/nvim-win64.zip" -o nvim-win64.zip
-  unzip nvim-win64
+  archive=$(download_release_first "nvim-win64.zip")
+  unzip "$archive"
 fi
 
